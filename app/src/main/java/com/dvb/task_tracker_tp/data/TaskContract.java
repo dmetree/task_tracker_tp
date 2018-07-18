a4172acc1746e788fb8d29a7ba83cd2a9b663467
BondarenkoDmitry <demianbondarenko@gmail.com>
2018-07-18 17:29:40 +0300

    Adding DB

--- app/src/main/java/com/dvb/task_tracker_tp/data/TaskContract.java
+++ app/src/main/java/com/dvb/task_tracker_tp/data/TaskContract.java
@@ -1,4 +1,34 @@
 package com.dvb.task_tracker_tp.data;
 
+import android.content.ContentResolver;
+import android.net.Uri;
+import android.provider.BaseColumns;
+
 public class TaskContract {
+
+    private TaskContract(){}
+
+    public static final String CONTENT_AUTHORITY = "com.dvb.task_tracker_tp";
+    public static final Uri BASE_CONTENT_URI = Uri.parse("content://" + CONTENT_AUTHORITY);
+    public static final String PATH_TASK = "task";
+
+    public static final class TaskEntry implements BaseColumns {
+        public static final String CONTENT_ITEM_TYPE =
+                ContentResolver.CURSOR_ITEM_BASE_TYPE + "/" + CONTENT_AUTHORITY + "/" + PATH_TASK;
+
+        public static final Uri CONTENT_URI = Uri.withAppendedPath(BASE_CONTENT_URI, PATH_TASK);
+        public static final String CONTENT_LIST_TYPE =
+                ContentResolver.CURSOR_DIR_BASE_TYPE + "/" + CONTENT_AUTHORITY + "/" + PATH_TASK;
+
+        public final static String TABLE_NAME = "task";
+
+
+        public final static String _ID = BaseColumns._ID;
+
+        public final static String COLUMN_TASK_NAME = "name";
+        public final static String COLUMN_TASK_DETAILS = "details";
+        public final static String COLUMN_TASK_DEADLINE = "deadline";
+        public final static String COLUMN_TASK_STATUS = "status";
+
+    }
 }
--- app/src/main/java/com/dvb/task_tracker_tp/data/TaskDbHelper.java
+++ app/src/main/java/com/dvb/task_tracker_tp/data/TaskDbHelper.java
@@ -1,4 +1,33 @@
 package com.dvb.task_tracker_tp.data;
 
-public class TaskDbHelper {
+
+import android.content.Context;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+
+public class TaskDbHelper extends SQLiteOpenHelper {
+
+    public static final String DATABASE_NAME = "tasks.db";
+    public static final int DATABASE_VERSION = 1;
+
+    public TaskDbHelper(Context context) {
+        super(context, DATABASE_NAME, null, DATABASE_VERSION);
+    }
+
+    @Override
+    public void onCreate(SQLiteDatabase db) {
+        String SQL_CREATE_TASK_TABLE = "CREATE TABLE " + TaskContract.TaskEntry.TABLE_NAME + " ("
+                + TaskContract.TaskEntry._ID + " INTEGER PRIMARY KEY AUTOINCREMENT, "
+                + TaskContract.TaskEntry.COLUMN_TASK_NAME + " NOT NULL, "
+                + TaskContract.TaskEntry.COLUMN_TASK_DETAILS + " NOT NULL, "
+                + TaskContract.TaskEntry.COLUMN_TASK_DEADLINE + " NOT NULL, "
+                + TaskContract.TaskEntry.COLUMN_TASK_STATUS + " NOT NULL);";
+
+        db.execSQL(SQL_CREATE_TASK_TABLE);
+    }
+
+    @Override
+    public void onUpgrade(SQLiteDatabase db, int OldVersion, int newVersion) {
+
+    }
 }
